1c5130c56e0439927826e2f50751c254cca35c94
Justin Phillips <justinphil02@yahoo.com>
2019-10-01 01:29:27 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,30 @@
+def using_push (a, b)
+  a.push(b)
+end
+
+def using_unshift (a,b)
+  a.unshift(b)
+end
+
+def using_pop (a)
+  a.pop
+end
+
+def pop_with_args(a)
+  b = a.pop 
+  c = a.pop
+  d = [b, c]
+end
+
+def using_shift (a)
+  a.shift
+end
+
+def shift_with_args (a)
+  b = a.shift 
+  c = a.shift
+  d = [b,c]
+end
+
+def using_concat (a,b)
+  
